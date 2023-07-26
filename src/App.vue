de7edd194bc956f51b86b3019b5a341458ddf17f
novack-fabio <fabio.m2@hotmail.com>
2023-07-25 22:14:39 -0300

    Teste

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <h1>Cat Size</h1>
+    <Cat></Cat>
+  </div>
+</template>
+
+<script>
+import Cat from './components/Cat'
+
+export default {
+  name: 'App',
+  components: {
+    Cat
+  }
+}
+</script>
+
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
